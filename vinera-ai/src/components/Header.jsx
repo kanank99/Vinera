d34b684ddcbd587ec412a6edbc1559fb3b31761f
Kanan Kontorovsky <kanankontorovsky@hotmail.com>
2023-07-15 14:23:53 -0400

    Updated Logo png and fixed Main gap distance.

--- vinera-ai/src/components/Header.jsx
+++ vinera-ai/src/components/Header.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import Logo from '../assets/images/vinera.png';
+import Logo from '../assets/images/1.png';
 import Menu from '../assets/images/menu.png';
 import Dropdown from '../assets/images/dropdown.png';
 import Open from '../assets/images/open.png';
 
 function Header() {
-  return (
-    <div className='text-[#191c1d] bg-[#F7F7F7] fixed w-full flex justify-between h-20 items-center pl-4 pr-4 border-b-2 lg:pl-20 lg:pr-20 z-50'>
-        <div className='flex items-center'>
-            <div> <img src={Logo} alt='logo' className='w-20 h-20' /> </div>
-            <div className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-green-300 to-green-900'>Vinera</div>
-        </div>
-        <div className='flex gap-5'>
-            {/* Code for dropdown */}
-            <div className='hidden sm:flex sm:gap-5 sm:items-center sm:justify-center'>
+    return (
+        <div className='text-[#191c1d] bg-[#F7F7F7] fixed w-full flex justify-between h-20 items-center pl-4 pr-4 border-b-2 lg:pl-20 lg:pr-20 z-50'>
+            <div className=''>
+                <div> <img src={Logo} alt='logo' className='w-40 h-40 pt-5' /> </div>
+            </div>
+            <div className='flex gap-5'>
+                {/* Code for dropdown */}
+                <div className='hidden sm:flex sm:gap-5 sm:items-center sm:justify-center'>
                     <div className='hidden lg:flex text-xl bg-[#f7f7f7f7] p-2 rounded-xl h-full border-2 cursor-pointer justify-center items-center gap-2'><p className='tracking-tight'>Solutions</p><img src={Dropdown} alt='dropdown' className='w-5 h-5' /></div>
                     <div className='hidden lg:flex text-xl bg-[#f7f7f7f7] p-2 rounded-xl h-full border-2 cursor-pointer justify-center items-center gap-2'><p className='tracking-tight'>Our Company</p><img src={Dropdown} alt='dropdown' className='w-5 h-5' /></div>
                     <div className='text-xl border border-[#313435] p-2 rounded-xl h-full transition hover:scale-[1.02] cursor-pointer flex justify-center items-center hover-color-change'>
@@ -24,12 +23,12 @@ function Header() {
                         <img src={Open} alt='dropdown' className='w-5 h-5 scale-110 z-10' />
                     </div>
                 </div>
-            <div className='lg:hidden bg-[#19d6a752] p-2 rounded-xl sm:gap-2 flex justify-center items-center'>
-                <img src={Menu} alt='menu' className='sm:w-6 sm:h-6 w-10 h-10' />
+                <div className='lg:hidden bg-[#19d6a752] p-2 rounded-xl sm:gap-2 flex justify-center items-center'>
+                    <img src={Menu} alt='menu' className='sm:w-6 sm:h-6 w-10 h-10' />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
--- vinera-ai/src/components/Main.jsx
+++ vinera-ai/src/components/Main.jsx
@@ -51,7 +51,7 @@ function Main() {
       strings: ['Chatbots.^1000', 'Content Generation.^1000', 'Marketing Campaigns.^1000', 'Customer Service.^1000', 'Solutions.'],
       typeSpeed: 90,
     });
-  
+
     return () => {
       // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
@@ -61,25 +61,25 @@ function Main() {
   return (
     <>
       <div className='relative vanta font-display flex justify-center items-center h-[100dvh]'>
-          <div className='z-20 flex flex-col justify-center items-center sm:gap-20'>              <p className='text-4xl h-60 sm:text-6xl leading-snug sm:leading-snug font-bold text-center w-[90%] pt-10'>Develop <span className='text-transparent bg-clip-text bg-gradient-to-tr from-green-200 to-green-800'>AI</span> Powered <span className='text-transparent bg-clip-text bg-gradient-to-t from-green-800 to-green-100'>Automated</span> <br /> <span ref={ele} /></p>
-              <p className='text-center leading-snug w-[90%] text-2xl font-normal' >Leverage Vinera AI to <span className='font-semibold'>automate tasks</span>, eradicate manual work, and achieve <span className='font-semibold'>significant cost savings</span>.</p>
-              <div className='text-center leading-snug w-[90%] text-2xl font-normal flex justify-center items-center gap-2 pt-14 sm:pt-0' >
-                <p className='tracking-tight'>4.8/5.0</p> 
-                <img src={Reviews} className='h-7 w-7 bg-[#f7f7f7]' alt='reviews'/>
-                <p className='tracking-tight'>Rating</p>
-                <p className='tracking-tight text-base place-self-end'>(based on client reviews)</p>
-              </div>
-              <div className='flex gap-10'>
-                  <div className='glow-button mt-20 sm:mt-0 text-xl p-3 rounded-xl h-full transition hover:scale-[1.02] cursor-pointer flex justify-center items-center hover-color-change'>
-                      <div className='relative z-10 font-semibold'>GET STARTED</div>
-                  </div>
-                  <div className='glow-button mt-20 bg-[#f7f7f7] sm:mt-0 text-xl p-3 rounded-xl h-full transition hover:scale-[1.02] cursor-pointer flex justify-center items-center border-2'>
-                      <div className='relative z-10 font-semibold'>Learn More</div>
-                  </div>
-              </div>
-                  
+        <div className='z-20 flex flex-col justify-center items-center sm:gap-14'>              <p className='text-4xl h-60 sm:text-6xl leading-snug sm:leading-snug font-bold text-center w-[90%] pt-10'>Develop <span className='text-transparent bg-clip-text bg-gradient-to-tr from-green-200 to-green-800'>AI</span> Powered <span className='text-transparent bg-clip-text bg-gradient-to-t from-green-800 to-green-100'>Automated</span> <br /> <span ref={ele} /></p>
+          <p className='text-center leading-snug w-[90%] text-2xl font-normal' >Leverage Vinera AI to <span className='font-semibold'>automate tasks</span>, eradicate manual work, and achieve <span className='font-semibold'>significant cost savings</span>.</p>
+          <div className='text-center leading-snug w-[90%] text-2xl font-normal flex justify-center items-center gap-2 pt-14 sm:pt-0' >
+            <p className='tracking-tight'>4.8/5.0</p>
+            <img src={Reviews} className='h-7 w-7 bg-[#f7f7f7]' alt='reviews' />
+            <p className='tracking-tight'>Rating</p>
+            <p className='tracking-tight text-base place-self-end'>(based on client reviews)</p>
           </div>
-          <div className='absolute h-full w-full' id='vanta' ref={vantaRef}></div>
+          <div className='flex gap-10'>
+            <div className='glow-button mt-20 sm:mt-0 text-xl p-3 rounded-xl h-full transition hover:scale-[1.02] cursor-pointer flex justify-center items-center hover-color-change'>
+              <div className='relative z-10 font-semibold'>GET STARTED</div>
+            </div>
+            <div className='glow-button mt-20 bg-[#f7f7f7] sm:mt-0 text-xl p-3 rounded-xl h-full transition hover:scale-[1.02] cursor-pointer flex justify-center items-center border-2'>
+              <div className='relative z-10 font-semibold'>Learn More</div>
+            </div>
+          </div>
+
+        </div>
+        <div className='absolute h-full w-full' id='vanta' ref={vantaRef}></div>
       </div>
     </>
   )
