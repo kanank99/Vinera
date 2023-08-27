86b817b95a69399b85af70a804cf7aa3005f7dca
Kanan Kontorovsky <kanankontorovsky@hotmail.com>
2023-08-27 00:49:10 -0400

    Built/Styled AutomationsHeader Component

--- vinera-ai/src/components/Automations.jsx
+++ vinera-ai/src/components/Automations.jsx
@@ -1,41 +1,37 @@
 import React from 'react'
+import { useEffect } from 'react';
 import Integrations from './Integrations';
+import AutomationsHeader from './AutomationsHeader';
 
 function Automations(props) {
+
+    useEffect(() => {
+        const primaryHeader = document.querySelector(".automations-header");
+        const scrollWatcher = document.createElement("div");
+
+        scrollWatcher.setAttribute("data-scroll-watcher", "");
+        primaryHeader.before(scrollWatcher);
+
+        //change color of nav bar when scrolling
+
+        const navObserver = new IntersectionObserver(
+            (entries) => {
+                primaryHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+            },
+            { rootMargin: "50px 0px 0px 0px" }
+        );
+
+        navObserver.observe(scrollWatcher);
+
+
+
+    }, [])
+
     return (
         <div>
             <Integrations darkMode={props.darkMode} />
-            {/* build a sticky header */}
-            {/* // bg-[#131315] */}
-            <div className='sticky z-50  top-[72px] py-[32px] text-white font-display'>
-                <div className='relative flex  items-center max-w-[1008px] w-full mx-auto'>
-
-                    <div className='flex items-center justify-between gap-20 lg:gap-2 w-full lg:overflow-hidden overflow-x-scroll'>
-                        <div className='flex items-center gap-2'>
-                            <img className='' alt="" data-nosnippet="true" data-loaded="true" loading="lazy" width="22" height="22" decoding="async" data-nimg="1" class="sc-5f7b9831-0 dqWqYY" src="https://cdn.sanity.io/images/ornj730p/production/aae450ebc16ab7686096745ac69aafabac309c41-64x64.png?q=95&amp;auto=format&amp;dpr=2" />
-                            <h1 className='whitespace-nowrap'>Customer Service</h1>
-                        </div>
-                        <div className='flex items-center gap-2'>
-                            <img className='' alt="" data-nosnippet="true" data-loaded="true" loading="lazy" width="22" height="22" decoding="async" data-nimg="1" class="sc-5f7b9831-0 dqWqYY" src="https://cdn.sanity.io/images/ornj730p/production/aae450ebc16ab7686096745ac69aafabac309c41-64x64.png?q=95&amp;auto=format&amp;dpr=2" />
-                            <h1 className='whitespace-nowrap'>Lead Generation</h1>
-                        </div>
-                        <div className='flex items-center gap-2'>
-                            <img className='' alt="" data-nosnippet="true" data-loaded="true" loading="lazy" width="22" height="22" decoding="async" data-nimg="1" class="sc-5f7b9831-0 dqWqYY" src="https://cdn.sanity.io/images/ornj730p/production/aae450ebc16ab7686096745ac69aafabac309c41-64x64.png?q=95&amp;auto=format&amp;dpr=2" />
-                            <h1 className='whitespace-nowrap'>Data</h1>
-                        </div>
-                        <div className='flex items-center gap-2'>
-                            <img className='' alt="" data-nosnippet="true" data-loaded="true" loading="lazy" width="22" height="22" decoding="async" data-nimg="1" class="sc-5f7b9831-0 dqWqYY" src="https://cdn.sanity.io/images/ornj730p/production/aae450ebc16ab7686096745ac69aafabac309c41-64x64.png?q=95&amp;auto=format&amp;dpr=2" />
-                            <h1 className='whitespace-nowrap'>Marketing</h1>
-                        </div>
-                        <div className='flex items-center gap-2'>
-                            <img className='' alt="" data-nosnippet="true" data-loaded="true" loading="lazy" width="22" height="22" decoding="async" data-nimg="1" class="sc-5f7b9831-0 dqWqYY" src="https://cdn.sanity.io/images/ornj730p/production/aae450ebc16ab7686096745ac69aafabac309c41-64x64.png?q=95&amp;auto=format&amp;dpr=2" />
-                            <h1 className='whitespace-nowrap'>Process Automations</h1>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
+            <AutomationsHeader darkMode={props.darkMode} />
+            <div className='h-[10000px] bg-[#060606]'></div>
         </div>
     )
 }
--- vinera-ai/src/components/Integrations.jsx
+++ vinera-ai/src/components/Integrations.jsx
@@ -7,7 +7,7 @@ import Mailchimp from '../assets/images/mailchimp-img.png';
 import Gmail from '../assets/images/gmail-icon.svg';
 import Drive from '../assets/images/drive-icon.svg';
 import Slack from '../assets/images/slack-img.png';
-import Sheets from '../assets/images/sheets-icon.png';
+import Teams from '../assets/images/teams-img.png';
 import Youtube from '../assets/images/youtube-icon.svg';
 import Twitter from '../assets/images/twitter-icon.svg';
 import Vinera from '../assets/images/vineraVblack.png';
@@ -81,27 +81,27 @@ function Integrations(props) {
                         <div className='noShowBall ball lg:flex lg:items-center lg:justify-center hidden lg:h-16 lg:w-16 lg:rounded-full lg:bg-[#000212] lg:dark:bg-[#ffffff0F] lg:text-white border-2 border-solid border-[#ffffff0D]'>
                             <img src={Youtube} alt='twitter-icon' className='hidden lg:flex lg:w-7 lg:h-7' />
                         </div>
-                        <div className='noShowBall ball md:flex md:items-center md:justify-center hidden md:w-14 md:h-14 lg:h-20 lg:w-20 md:rounded-full md:bg-[#000212] md:dark:bg-[#ffffff0F] md:text-white border-2 border-solid border-[#ffffff0D]'>
-                            <img src={Sheets} alt='google-sheets-icon' className='hidden md:flex md:w-6 md:h-8 lg:w-8 lg:h-12' />
+                        <div className='noShowBall ball sm:flex sm:items-center sm:justify-center hidden sm:w-14 sm:h-14 lg:h-20 lg:w-20 sm:rounded-full sm:bg-[#000212] sm:dark:bg-[#ffffff0F] sm:text-white border-2 border-solid border-[#ffffff0D]'>
+                            <img src={Teams} alt='microsoft-teams-icon' className='hidden sm:flex sm:w-6 sm:h-6 lg:w-10 lg:h-10' />
                         </div>
-                        <div className='noShowBall ball flex items-center justify-center h-16 w-16 lg:h-24 lg:w-24 rounded-full bg-[#000212] dark:bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
-                            <img src={Facebook} alt='facebook-icon' className='w-10 h-10 lg:w-12 lg:h-12' />
+                        <div className='noShowBall ball flex items-center justify-center h-14 w-14 sm:w-16 sm:h-16 lg:h-24 lg:w-24 rounded-full bg-[#000212] dark:bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
+                            <img src={Facebook} alt='facebook-icon' className='w-7 h-7 sm:w-9 sm:h-9 lg:w-12 lg:h-12' />
                         </div>
-                        <div className='noShowBall ball flex items-center justify-center h-20 w-20 lg:h-32 lg:w-32 rounded-full bg-[#000212] dark:bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
-                            <img src={Mailchimp} alt='mailchimp-icon' className='w-10 h-10 lg:w-20 lg:h-20' />
+                        <div className='noShowBall ball flex items-center justify-center h-16 w-16 sm:w-20 sm:h-20 lg:h-32 lg:w-32 rounded-full bg-[#000212] dark:bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
+                            <img src={Mailchimp} alt='mailchimp-icon' className='w-8 h-8 sm:w-12 sm:h-12 lg:w-20 lg:h-20' />
                         </div>
-                        <div className='noShowBall ball flex items-center justify-center h-24 w-24 lg:h-40 lg:w-40 rounded-full bg-gradient-to-tr from-green-600 to-green-900 dark:from-[#455eb5] dark:to-purple-600 text-white'>
-                            <img src={`${props.darkMode ? Vinerawhite : Vinera}`} alt='vinera-icon' className='w-16 h-16 lg:w-24 lg:h-24 lg:scale-150' />
+                        <div className='noShowBall ball flex items-center justify-center h-20 w-20 sm:w-24 sm:h-24 lg:h-40 lg:w-40 rounded-full bg-gradient-to-tr from-green-600 to-green-900 dark:from-[#455eb5] dark:to-purple-600 text-white'>
+                            <img src={`${props.darkMode ? Vinerawhite : Vinera}`} alt='vinera-icon' className='w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24 lg:scale-150' />
                             <div className={`${props.darkMode ? 'space' : 'null'} flex items-center justify-center rounded-full bg-gradient-to-bl text-white`}></div>
                         </div>
-                        <div className='noShowBall ball flex items-center justify-center h-20 w-20 lg:h-32 lg:w-32 rounded-full bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
-                            <img src={Gmail} alt='gmail-icon' className='w-10 h-10 lg:w-16 lg:h-16' />
+                        <div className='noShowBall ball flex items-center justify-center h-16 w-16 sm:w-20 sm:h-20 lg:h-32 lg:w-32 rounded-full bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
+                            <img src={Gmail} alt='gmail-icon' className='w-8 h-8 sm:w-12 sm:h-12 lg:w-16 lg:h-16' />
                         </div>
-                        <div className='noShowBall ball flex items-center justify-center h-16 w-16 lg:h-24 lg:w-24 rounded-full bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
-                            <img src={Drive} alt='drive-icon' className='w-10 h-10 lg:w-14 lg:h-14' />
+                        <div className='noShowBall ball flex items-center justify-center h-14 w-14 sm:w-16 sm:h-16 lg:h-24 lg:w-24 rounded-full bg-[#ffffff0F] text-white border-2 border-solid border-[#ffffff0D]'>
+                            <img src={Drive} alt='drive-icon' className='w-7 h-7 sm:w-9 sm:h-9 lg:w-14 lg:h-14' />
                         </div>
-                        <div className='noShowBall ball md:flex md:items-center md:justify-center hidden md:w-14 md:h-14 lg:h-20 lg:w-20 md:rounded-full md:bg-[#ffffff0F] md:text-white border-2 border-solid border-[#ffffff0D]'>
-                            <img src={Slack} alt='drive-icon' className='hidden md:flex md:w-7 md:h-7 lg:w-10 lg:h-10' />
+                        <div className='noShowBall ball sm:flex sm:items-center sm:justify-center hidden sm:w-14 sm:h-14 lg:h-20 lg:w-20 sm:rounded-full sm:bg-[#ffffff0F] sm:text-white border-2 border-solid border-[#ffffff0D]'>
+                            <img src={Slack} alt='drive-icon' className='hidden sm:flex sm:w-7 sm:h-7 lg:w-10 lg:h-10' />
                         </div>
                         <div className='noShowBall ball lg:flex lg:items-center lg:justify-center hidden lg:h-16 lg:w-16 lg:rounded-full lg:bg-[#ffffff0F] lg:text-white border-2 border-solid border-[#ffffff0D]'>
                             <img src={Twitter} alt='twitter-icon' className='hidden lg:flex lg:w-7 lg:h-7' />
